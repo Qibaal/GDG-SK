5265e12922c8a26c6c78f4f872dc7bc0e2c168c4
cupski <yusufrafi77@gmail.com>
2025-05-14 20:57:01 +0700

    feat: google sign in

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:logger/logger.dart';
 
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(
